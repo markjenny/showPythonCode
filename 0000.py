9f8aad23f3b09bb41edd8c2f4a7df830d36877b5
Mark <lijianlin1990@sina.com>
2015-12-29 21:25:49 +0800

    modified the 0000.py

--- 0000.py
+++ 0000.py
@@ -1,12 +1,23 @@
 # -*- coding = utf=8 -*-
 'the first exercise'
 
-__author__ = mark
+__author__ = 'mark'
 
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
-im = open('sec.jpg')
-w,h = im.size
-print('Original image size: %s%s' % (w, h))
+def add_num():
+	#打开图片
+	im = Image.open('sex.jpg')
+	draw = ImageDraw.Draw(im)
 
-im.rotate(45).show()
+	#设定图片上要写的字体
+	myfont = ImageFont.truetype('C:/windows/fonts/Arial.ttf',size = 40)
+	fillcolor = '#ff0000'
+
+
+	width, heigh = im.size
+	draw.text((width-40, 50), '99', font = myfont, fill=fillcolor)
+	im.save('sex01.jpg', 'jpeg')
+
+if __name__=='__main__':
+	add_num()
